f0a031a00fcc33e12947cd4106faf8172252fa2c
TheManWithGut <ictburt@gmail.com>
2025-03-04 12:06:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
+
+
+
 const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
-const keep_alive = require('./keep_alive.js');
+const keep_alive = require('./keep_alive.js')
 
-
-const username = process.env.username;
-const password = process.env.password;
-const shared_secret = process.env.shared;
+var username = process.env.username;
+var password = process.env.password;
+var shared_secret = process.env.shared;
 
 const games = [730, 440, 365670, 218620, 612880, 356190, 4000, 418370, 359550, 1237970, 1659040, 1436990, 2157110];
-const status = 7; // 1 - online, 7 - invisible
+var status = 1;  // 1 - online, 7 - invisible
 
-const user = new steamUser();
 
-// Přihlášení uživatele
-user.logOn({
-    accountName: username,
-    password: password,
-    twoFactorCode: steamTotp.generateAuthCode(shared_secret)
-});
-
+user = new steamUser();
+user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
 user.on('loggedOn', () => {
-    console.log(`Úspěšně přihlášen jako ${user.steamID}`);
-    user.setPersona(status);
-    user.gamesPlayed(games);
-});
-
-user.on('error', (err) => {
-    console.error("Chyba při přihlášení:", err.message);
+	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
+	user.setPersona(status);               
+	user.gamesPlayed(games);
 });
